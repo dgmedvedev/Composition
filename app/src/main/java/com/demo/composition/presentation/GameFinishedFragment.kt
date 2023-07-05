6904951f784b115b44fdc7f54d68116cd1ff515b
dgmedvedev <dg-medvedev@mail.ru>
2023-07-05 14:11:38 +0300

    GameViewModel implemented

--- app/src/main/java/com/demo/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/demo/composition/presentation/GameFinishedFragment.kt
@@ -7,9 +7,9 @@ import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.demo.composition.R
 import com.demo.composition.databinding.FragmentGameFinishedBinding
 import com.demo.composition.domain.entity.GameResult
-import com.demo.composition.domain.entity.Level
 
 class GameFinishedFragment : Fragment() {
 
@@ -35,6 +35,11 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+        bindViews()
+    }
+
+    private fun setupClickListeners() {
         requireActivity().onBackPressedDispatcher.addCallback(
             viewLifecycleOwner,
             object : OnBackPressedCallback(true) {
@@ -50,6 +55,43 @@ class GameFinishedFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_score),
+                gameResult.gameSettings.minCountOfCorrectAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfCorrectAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfCorrectAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                calculatePercentOfCorrectAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+    }
+
+    private fun calculatePercentOfCorrectAnswers() = with(gameResult) {
+        if (countOfQuestions == 0) {
+            0
+        } else
+            ((countOfCorrectAnswers / countOfQuestions.toDouble()) * 100).toInt()
     }
 
     private fun parseArgs() {
